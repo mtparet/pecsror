f20bb99a74470295233651838d28d4f60a892a0b
mtparet3 <mtparet3@mtparet3.(none)>
2011-12-16 22:42:05 +0100

    sauvegarde

--- app/controllers/affichage_controller.rb
+++ app/controllers/affichage_controller.rb
@@ -1,7 +1,22 @@
+require 'espeak-ruby'
+
+include ESpeak
+
 class AffichageController < ApplicationController
 
 	def index
 		@list_categorie = Images.all
+	end
+
+	def create_audio
+		espeak("test.mp3", :text => "Hello World")
+		@audio = "test.mp3"
+		
+		respond_to do |format|
+			format.html { redirect_to('affichage') }
+			format.js
+			format.xml {head :ok}
+		end
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Pecs::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
    root :to => 'affichage#index'
+	match "/create_audio/" => "affichage#create_audio"
 
   # See how all your routes lay out with "rake routes"
 
