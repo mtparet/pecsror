1801ff1b0b3638696335a9e84e66f24e1f299d5c
Matthieu Paret <matthieuparet69@gmail.com>
2012-02-07 09:37:40 +0100

    fix bug by delete Logger in development config environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Pecs::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
- config.logger = Logger.new(STDOUT)
-config.logger = Log4r::Logger.new("Application Log"
+  #config.logger = Logger.new(STDOUT)
+  #config.logger = Log4r::Logger.new("Application Log")
  
 end
