50c07fdf6a6fe54ed01f35f7f54e3354b2673576
Matthieu Paret <matthieuparet69@gmail.com>
2012-02-16 15:42:49 +0100

    nettoyage de fichiers

--- app/models/images.rb
+++
@@ -1,7 +0,0 @@
-require 'carrierwave/orm/activerecord'
-
-class Images < ActiveRecord::Base
-  has_many :sequence_liste
-  has_many :sequence, :through => :sequence_liste
-  mount_uploader :image_file, ImageFileUploader
-end
