01103add42cf00a344e9fc7d6a45b044f6739c45
Matthieu Paret <matthieuparet69@gmail.com>
2012-02-18 20:29:34 +0100

    ajout d'une home TRES basic ajout d'un lien de retour sur les pages

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Pecs::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'affichage#index'
+  root :to => 'home#index'
   
   match "/images/new" => "image#new", :as => :image
   get "/images" => "image#index"
